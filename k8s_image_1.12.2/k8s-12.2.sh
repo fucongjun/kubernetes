114793a436f2e44f33155b697ea594035c6edbc8
不落教青年 <41889276+fucongjun@users.noreply.github.com>
2018-12-20 11:26:43 +0800

    Add files via upload

---
+++ k8s_image_1.12.2/k8s-12.2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "正在拉取镜像"
+docker pull mirrorgooglecontainers/kube-apiserver-amd64:v1.12.2
+docker pull mirrorgooglecontainers/kube-controller-manager-amd64:v1.12.2
+docker pull mirrorgooglecontainers/kube-scheduler-amd64:v1.12.2
+docker pull mirrorgooglecontainers/kube-proxy-amd64:v1.12.2
+docker pull mirrorgooglecontainers/etcd-amd64:3.2.24
+docker pull mirrorgooglecontainers/pause-amd64:3.1
+docker pull mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.10.0
+docker pull mirrorgooglecontainers/metrics-server-amd64:v0.3.1
+docker pull coredns/coredns:1.2.2
+echo "给镜像添加官网标签"
+docker tag mirrorgooglecontainers/kube-apiserver-amd64:v1.12.2  k8s.gcr.io/kube-apiserver:v1.12.2
+docker tag mirrorgooglecontainers/kube-controller-manager-amd64:v1.12.2  k8s.gcr.io/kube-controller-manager:v1.12.2 
+docker tag mirrorgooglecontainers/kube-scheduler-amd64:v1.12.2  k8s.gcr.io/kube-scheduler:v1.12.2
+docker tag mirrorgooglecontainers/kube-proxy-amd64:v1.12.2  k8s.gcr.io/kube-proxy:v1.12.2
+docker tag mirrorgooglecontainers/etcd-amd64:3.2.24  k8s.gcr.io/etcd:3.2.24
+docker tag mirrorgooglecontainers/pause-amd64:3.1  k8s.gcr.io/pause:3.1
+docker tag mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.10.0  k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0 
+docker tag mirrorgooglecontainers/metrics-server-amd64:v0.3.1  k8s.gcr.io/metrics-server-amd64:v0.3.1
+docker tag coredns/coredns:1.2.2  k8s.gcr.io/coredns:1.2.2
+echo "删除原有镜像"
+docker image rm mirrorgooglecontainers/kube-apiserver-amd64:v1.12.2
+docker image rm mirrorgooglecontainers/kube-controller-manager-amd64:v1.12.2
+docker image rm mirrorgooglecontainers/kube-scheduler-amd64:v1.12.2
+docker image rm mirrorgooglecontainers/kube-proxy-amd64:v1.12.2
+docker image rm mirrorgooglecontainers/etcd-amd64:3.2.24
+docker image rm mirrorgooglecontainers/pause-amd64:3.1
+docker image rm mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.10.0
+docker image rm mirrorgooglecontainers/metrics-server-amd64:v0.3.1
+docker image rm coredns/coredns:1.2.2
